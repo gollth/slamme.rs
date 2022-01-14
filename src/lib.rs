b1f9428d8cfbe6c00c63b8514c7e676a282f34b7
Thore Goll <thore.goll@franka.de>
2022-01-14 15:27:43 +0100

    ADD: Update Step of EKF

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
 use ndarray::prelude::*;
+use ndarray_linalg::Inverse;
+use rand::thread_rng;
+use rand_distr::{Distribution, Normal};
 
 pub type Matrix = Array2<f32>;
 pub type Vector = Array1<f32>;
 
 pub type Belief = (Vector, Matrix);
 pub type World = Array2<u8>; // TODO u8 -> ???
+
+// hidden real observation noise
+const SENSOR_LANDMARK_NOISE: f32 = 0.2425;
+const SENSOR_WHEEL_NOISE: f32 = 0.1313;
 
 pub fn predict(
     x_hat: &Vector,
@@ -17,6 +24,39 @@ pub fn predict(
     let x_hat_prime = f.dot(x_hat) + b.dot(u);
     let p_hat_prime = f.dot(p_hat).dot(&f.t()) + Matrix::from_diag(q);
     (x_hat_prime, p_hat_prime)
+}
+
+pub fn update(z: &Vector, h: &Matrix, x_hat: &Vector, p: &Matrix, v: &Vector) -> Belief {
+    let y_hat = z - h.dot(x_hat); // noisy measurement
+    let s = h.dot(p).dot(&h.t()) + Matrix::from_diag(v);
+    let k = p.dot(&h.t()).dot(&s.inv().unwrap()); // optimal Kalman gain
+    println!("Kalman Gain:\n{}", k);
+    let x_hat_prime = x_hat + k.dot(&y_hat);
+    let p_prime = (Matrix::eye(p.dim().0) - k.dot(h)).dot(p);
+    (x_hat_prime, p_prime)
+}
+
+pub fn measure(world: &World, x_hat: &Vector) -> Vector {
+    let mut rng = thread_rng();
+    let mut z = Vec::new();
+    let landmarks = Normal::new(0., SENSOR_LANDMARK_NOISE).unwrap();
+    let wheels = Normal::new(0., SENSOR_WHEEL_NOISE).unwrap();
+
+    // Add the robot state to the measurement
+    for i in 0..4 {
+        z.push(x_hat[i] + wheels.sample(&mut rng));
+    }
+
+    // Add the landmarks to the measurement
+    for j in 0..world.nrows() {
+        for i in 0..world.ncols() {
+            if world[[j, i]] > 0 {
+                z.push(i as f32 + landmarks.sample(&mut rng));
+                z.push(j as f32 + landmarks.sample(&mut rng));
+            }
+        }
+    }
+    Vector::from_vec(z)
 }
 
 fn inside_ellipse(point: &Vector, center: &Vector, axes: &ArrayView1<f32>) -> bool {
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use ndarray::{concatenate, prelude::*};
-use slammers::{draw, predict, Matrix, Vector};
+use slammers::{draw, measure, predict, update, Matrix, Vector};
 use std::io::stdin;
 use termion::input::TermRead;
+
+const NOISE_OBSERVATION_SIGMA: f32 = 0.01;
+const NOISE_PROCESS_SIGMA: f32 = 0.05;
 
 fn main() {
     const K: usize = 3; // number of landmarks
@@ -34,7 +37,7 @@ fn main() {
         concatenate![Axis(1), prr0, Matrix::zeros((4, N))],
         concatenate![Axis(1), Matrix::zeros((N, 4)), Matrix::zeros((N, N))]
     ];
-    println!("P: {}", p0);
+    println!("P: {:?}", p0.dim());
 
     // Dynamics Model
     let f = concatenate![
@@ -51,30 +54,30 @@ fn main() {
         ],
         concatenate![Axis(1), Matrix::zeros((N, 4)), Matrix::eye(N)],
     ];
-    println!("F: {}", f);
+    println!("F: {:?}", f.dim());
+
+    // process noise
+    let w = NOISE_PROCESS_SIGMA * Vector::ones(4 + N);
+    println!("w: {}", w);
 
     // How do control inputs apply to state?
     let b = concatenate![
         Axis(0),
-        array![[1., 0.], [0., 1.], [0., 0.], [0., 0.],],
-        Matrix::zeros((N, 2)),
+        array![[1., 0.], [0., 1.], [0., 0.], [0., 0.],], // robot is moved on position level
+        Matrix::zeros((N, 2)),                           // landmarks aren't affected by controls
     ];
-    println!("B: {}", b);
+    println!("B: {:?}", b.dim());
 
     // Observation Model
-    // TODO
-
-    // observation noise
-    // TODO
-    // let v = array![[1., 0., 0., 0.], [0., 1., 0., 0.]];
-
-    // process noise
-    let w = concatenate![
+    let h = concatenate![
         Axis(0),
-        array![0.01, 0.01, 0.01, 0.01], // robot state
-        0.01 * Vector::ones(N),         // landmarks
+        Matrix::zeros((4, N + 4)),
+        concatenate![Axis(1), Matrix::zeros((N, 4)), Matrix::eye(N)]
     ];
-    println!("w: {}", w);
+    println!("H: {:?}", h.dim());
+
+    // Observation noise
+    let v = NOISE_OBSERVATION_SIGMA * Vector::ones(4 + N);
 
     // Add some obstacles to the world
     let mut world = Array2::zeros((16, 32));
@@ -85,8 +88,6 @@ fn main() {
         );
         world[coord] = 1;
     }
-    world[[10, 2]] = 2;
-    world[[11, 11]] = 2;
 
     let u = array![1., 0.]; // Control inputs
 
@@ -97,5 +98,11 @@ fn main() {
         stdin().keys().next();
 
         belief = predict(&belief.0, &belief.1, &f, &u, &b, &w);
+        draw(0, &world, &belief.0, &belief.1, &u);
+        stdin().keys().next();
+
+        let z = measure(&world, &belief.0);
+        belief = update(&z, &h, &belief.0, &belief.1, &v);
+        stdin().keys().next();
     }
 }
