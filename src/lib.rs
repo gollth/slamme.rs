fedfbedcc95178683ec8a883dff7869ee320d77f
Thore Goll <thore.goll@franka.de>
2022-01-14 09:07:32 +0100

    ADD: Print diagnostics

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,13 @@ pub type Matrix = Array2<f32>;
 pub type Vector = Array1<f32>;
 
 pub type Belief = (Vector, Matrix);
-pub type Controls = Vector;
 pub type World = Array2<u8>; // TODO u8 -> ???
 
 pub fn predict(
     x_hat: &Vector,
     p_hat: &Matrix,
     f: &Matrix,
-    u: &Controls,
+    u: &Vector,
     b: &Matrix,
     q: &Matrix,
 ) -> Belief {
@@ -20,6 +19,17 @@ pub fn predict(
     (x_hat_prime, p_hat_prime)
 }
 
+fn inside_ellipse(point: &Vector, center: &Vector, axes: &ArrayView1<f32>) -> bool {
+    ((point[0] - center[0]).powf(2.) / axes[0].powf(2.)
+        + (point[1] - center[1]).powf(2.) / axes[1].powf(2.))
+        <= 1.
+}
+
+pub fn print(x_hat: &Vector, p_hat: &Matrix, u: &Vector) {
+    println!("x̂: {}", x_hat);
+    println!("P̂: {}", p_hat.diag().slice(s![0..]));
+    println!("û: {}", u);
+}
 pub fn draw(world: &World, x: &Vector, p: &Matrix) {
     print!("┌");
     for _ in 0..world.ncols() {
@@ -31,9 +41,7 @@ pub fn draw(world: &World, x: &Vector, p: &Matrix) {
         for i in 0..world.ncols() {
             if x[0].round() as usize == i && x[1].round() as usize == j {
                 print!("⊕");
-            } else if (x[0] - i as f32).powf(2.) <= p[[0, 0]].powf(2.)
-                || (x[1] - j as f32).powf(2.) <= p[[1, 1]].powf(2.)
-            {
+            } else if inside_ellipse(&array![i as f32, j as f32], &x, &p.diag().slice(s![0..=1])) {
                 print!("·");
             } else {
                 print!(" ");
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use ndarray::prelude::*;
-use slammers::draw;
-use slammers::predict;
+use slammers::{draw, predict, print};
 
 fn main() {
     // initial state
@@ -25,24 +24,28 @@ fn main() {
     let b = array![[0., 0.], [0., 0.], [1., 0.], [0., 1.],];
 
     // process noise
-    let q = Array2::from_diag(&array![0.05, 0.05, 0.01, 0.01]);
+    let q = Array2::from_diag(&array![0.01, 0.01, 0.01, 0.01]);
 
-    let board = array![
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ];
+    let board = Array2::zeros((16, 32));
 
     let u = array![1., 0.]; // Control inputs
 
     let mut belief = (x0, p0);
 
+    println!("Step 0:");
     draw(&board, &belief.0, &belief.1);
+    print(&belief.0, &belief.1, &u);
+    println!("------------------------");
+
+    println!("Step 1:");
     belief = predict(&belief.0, &belief.1, &f, &u, &b, &q);
     draw(&board, &belief.0, &belief.1);
+    print(&belief.0, &belief.1, &u);
+    println!("------------------------");
+
+    println!("Step 2:");
     belief = predict(&belief.0, &belief.1, &f, &u, &b, &q);
     draw(&board, &belief.0, &belief.1);
+    print(&belief.0, &belief.1, &u);
+    println!("------------------------");
 }
